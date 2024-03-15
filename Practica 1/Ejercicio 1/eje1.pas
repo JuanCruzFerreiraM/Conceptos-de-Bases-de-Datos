05aa9e7b204bc53a910a8c96c6285b00f081ab83
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2024-03-15 17:00:35 -0300

    Se agrega la solución del ejercicio 1

---
+++ Practica 1/Ejercicio 1/eje1.pas
@@ -0,0 +1,29 @@
+
+Program eje1;
+
+Const keyWord = 'cemento';
+  //Palbra que al ingresarse concluye con la carga de valores al archivo.
+
+Type 
+
+  textFile = file Of string[255];
+
+Var 
+
+  newFile: textFile;
+  fileName: string[25];
+  newInputWord: string[255];
+
+
+Begin
+    writeln('Ingrese el nombre que le desea colocar al archivo: ');
+    Read(fileName);
+    Assign(newFile, fileName);
+    Rewrite(newFile);
+    repeat
+      Write('Ingrese el material que desea agregar al archivo: '); //Preguntar porque se produce el problema de la doble escritura.
+      ReadLn(newInputWord);
+      Write(newFile, newInputWord);
+    until (newInputWord = keyWord);
+    close(newFile);
+End.

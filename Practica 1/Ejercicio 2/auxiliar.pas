c4a103282255517c33846a5ac8c91894f74d5441
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2024-03-16 19:43:57 -0300

    Se agrega las resoluciones del ejercicio 2 Hay ciertas funcionalidades que en principio no funcionan.

---
+++ Practica 1/Ejercicio 2/auxiliar.pas
@@ -0,0 +1,27 @@
+//Nos permite crear el archivo de votantes usando valores randoms.
+
+Program auxiliar;
+
+Const municipalidades = 135;
+  //Lo hice por municipalidades para que sea mas simple.
+
+
+Var 
+  randNum: integer;
+  newFile: Text;
+  fileName: string[15];
+  i: Integer;
+
+Begin
+  Randomize;
+  fileName := 'votantes';
+  Assign(newFile, fileName);
+  Rewrite(newFile);
+  For i := 0 To municipalidades Do
+    Begin
+      randNum := Random(100) + 5;
+      //Suponemos que como minimo deben votar 5 y como maximo 105
+      Write(newFile, randNum);
+    End;
+  Close(newFile);
+End.
---
+++ Practica 1/Ejercicio 2/eje2.pas
@@ -0,0 +1,38 @@
+//Preguntar por que anda mal.
+Program eje2;
+
+Var 
+  arch: Text;
+  readValue: Integer;
+  minValue: Integer;
+  maxValue: Integer;
+  fileName: string[20];
+
+Begin
+  Write('Ingrese el nombre del archivo que desea procesar: ');
+  ReadLn(fileName);
+  If  (fileName <> '') Then
+    Begin
+      Assign(arch,fileName);
+      Reset(arch);
+      minValue := 9999;
+      maxValue := -1;
+      While (Not Eof(arch)) Do
+        Begin
+          Read(arch, readValue);
+          Write(readValue, ' - ');
+          If (readValue > maxValue) Then
+            Begin
+              maxValue := readValue;
+            End;
+          If (readValue < minValue )  Then
+            Begin
+              minValue := readValue;
+            End;
+        End;
+      WriteLn(' ');
+      WriteLn('La cantidad mas chica de votantes fue: ', minValue);
+      WriteLn('La cantidad mas grande de votantes fue: ', maxValue);
+    End;
+  Close(arch);
+End.

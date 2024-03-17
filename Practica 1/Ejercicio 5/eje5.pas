ff093479136e07d322bc892eb1c237a6edf2fd07
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2024-03-16 22:14:31 -0300

    Eje5 sin terminar

--- Practica 1/Ejercicio 5/eje5.pas
+++ Practica 1/Ejercicio 5/eje5.pas
@@ -42,7 +42,7 @@ Begin
     WriteLn('Archivo creado con exito');
 End;
 
-//Permite hacer un reporte via shell de la cantidad de especies, la altura maxima y la minima
+//Permite hacer un reporte via consola de la cantidad de especies, la altura maxima y la minima
 Procedure shellReport (Var spFile: flowerFile);
 
 Var 
@@ -51,18 +51,18 @@ Var
     cantSp:   Integer;
     output:   flower;
 Begin
-    cantSp =   0;
-    minH =   9999;
-    maxH =   -1;
+    cantSp :=   0;
+    minH :=   9999;
+    maxH :=   -1;
     Reset(spFile);
     While (Not Eof(spFile)) Do
         Begin
             Read(spFile,output);
-            cantSp  =   cantSp + 1;
+            cantSp  :=   cantSp + 1;
             If (output.height > maxH) Then maxH := output.height;
             If (output.height < minH) Then minH := output.height;
         End;
-    WriteLn('Cantidad de especies en el archivo: ', cantSp,'. Altura maxima: ',maxH,'. Altura minima: ',minH,'.');
+    WriteLn('Cantidad de especies en el archivo: ', cantSp,'. Altura maxima: ',maxH:4:3, '. Altura minima: ',minH:4:3,'.');
 End;
 
 //Permite modificar un nombre cientifico
@@ -76,11 +76,65 @@ Begin
     Reset(spFile);
     Write('Ingrese el nombre que desea cambiar: ');
     ReadLn(modName);
-    Repeat
-        Read(spFile, output);
-    Until (modName = output.scName) Or (Eof(spFile));
+    While (Not Eof(spFile)) Do
+        Begin
+            Read(spFile,output);
+            If (output.scName = modName) Then
+                Begin
+                    output.scName := modName;
+                    Seek(spFile, FilePos(spFile) - 1);
+                    Write(spFile,output);
+                    Break;
+                End;
+        End;
+    Close(spFile);
+    WriteLn('Modificacion realizada con exito');
+End;
 
+//Permite mostrar por consola el contenido del archivo
+Procedure catFile(Var spFile: flowerFile);
 
+Var 
+    output:   flower;
+Begin
+    Reset(spFile);
+    While (Not Eof(spFile)) Do
+        Begin
+            Read(spFile,output);
+            WriteLn('Numero de especie: ',output.numberSp,'. Altura maxima: ',output.height,'. Nombre cientifico: ',output.scName,'. Nombre vulgar: ',output.vgName,
+                    '. Color: ',output.color,'.');
+        End;
+    Close(spFile);
+End;
+
+//Permite agregar una nueva especie al archivo
+Procedure addNewSp(Var spFile: flowerFile);
+
+Var 
+    input:   flower;
+Begin
+    Reset(spFile);
+    While (Not Eof(spFile)) Do
+        Write('');
+    Seek(spFile,FilePos(spFile));
+    Write('Ingrese el nombre cientifico, para finalizar ingrese zzz: ');
+    ReadLn(input.scName);
+    While (input.scName <> 'zzz') Do
+        Begin
+            Write('Ingrese el numero de especie: ');
+            ReadLn(input.numberSp);
+            Write('Ingrese la altura maxima: ');
+            ReadLn(input.height);
+            Write('Ingrese el nombre vulgar de la especie: ');
+            ReadLn(input.vgName);
+            Write('Ingrese el color de la especie: ');
+            ReadLn(input.color);
+            Write(spFile,input);
+            Write('Ingrese el nombre cientifico, para finalizar ingrese zzz: ');
+            ReadLn(input.scName);
+        End;
+     Close(spFile);
+     WriteLn('Especie/es agreada/as con exito');   
 End;
 
 
@@ -92,7 +146,9 @@ End;
 Var 
     isFileCreated:   Boolean;
     option:   Integer;
+    spFile: flowerFile;
 Begin
+    Assign(spFile, 'especies');
     isFileCreated := False;
     While True Do
         Begin
@@ -117,13 +173,14 @@ Begin
                          If Not (isFileCreated) Then
                              Begin
                                  isFileCreated := True;
+                                 createFile(spFile);
                              End
                          Else WriteLn('El archivo de especies ya existe');
                      End;
-                2:   Write('');
-                3:   Write('');
-                4:   Write('');
-                5:   Write('');
+                2:   shellReport(spFile);
+                3:   catFile(spFile);
+                4:   modScName(spFile);
+                5:   addNewSp(spFile);
                 6:   Write('');
                 7:
                      Begin

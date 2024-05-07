150880c48d14d8d640d1ec338601395abd8c72ed
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2024-05-07 17:23:16 -0300

    Se agregan cambios en el ejercicio 5

--- Practica 2/Ejercicio 5/eje5.pas
+++ Practica 2/Ejercicio 5/eje5.pas
@@ -1,60 +1,76 @@
-program eje5;
 
-Const
-    VALORCORTE = 'zzz';
+Program eje5;
+
+Const 
+    VALORCORTE =   'zzz';
 
 Type 
-    reg = record
-        partido: string;
-        localidad: string;
-        barrio: string;
-        cant_n: integer;
-        cant_a: integer;
-    end;
+    reg =   Record
+        partido:   string;
+        localidad:   string;
+        barrio:   string;
+        cant_n:   integer;
+        cant_a:   integer;
+    End;
 
-    arch = file of reg;
+    arch =   file Of reg;
 
-    procedure leer(var f: arch; var r: reg);
-    begin
-      if (not Eof(f)) then
-        begin
-          Read(f,r);
-        end
-      else 
-        r.partido := VALORCORTE; 
-    end;
+Procedure leer(Var f: arch; Var r: reg);
+Begin
+    If (Not Eof(f)) Then
+        Begin
+            Read(f,r);
+        End
+    Else
+        r.partido := VALORCORTE;
+End;
 
-    procedure listar(var f: arch);
-    var 
-        r: reg;
-        aux_partido: string;
-        aux_localidad: string;
-        total_n: integer;
-        total_a: integer;
-        total_l_n: integer;
-        total_l_a: integer;
-    begin
-        Reset(f);
-        leer(f,r);
-        while (r.partido <> VALORCORTE) do
-          begin 
+Procedure listar(Var f: arch);
+
+Var 
+    r:   reg;
+    aux_partido:   string;
+    aux_localidad:   string;
+    total_n:   integer;
+    total_a:   integer;
+    total_l_n:   integer;
+    total_l_a:   integer;
+Begin
+    Reset(f);
+    leer(f,r);
+    While (r.partido <> VALORCORTE) Do
+        Begin
             aux_partido := r.partido;
             WriteLn('Partido: ',aux_partido);
             total_n := 0;
             total_a := 0;
-            while (aux_partido = r.partido) do
-              total_l_n := 0;
-              total_l_a := 0;
-              aux_localidad := r.localidad;
-              WriteLn('Localidad: ',aux_localidad);
-              while (aux_partido = r.partido) and (aux_localidad = r.localidad) do 
-                begin
-                  total_l_n := total_l_n + r.cant_n;
-                  total_l_a := total_l_a  + r.cant_a;
-                end;
-                
-          end;
-    end;
-begin
+            While (aux_partido = r.partido) Do
+                Begin
+                    total_l_n := 0;
+                    total_l_a := 0;
+                    aux_localidad := r.localidad;
+                    WriteLn('Localidad: ',aux_localidad);
+                    While (aux_partido = r.partido) And (aux_localidad = r.localidad) Do
+                        Begin
+                            total_l_n := total_l_n + r.cant_n;
+                            total_l_a := total_l_a  + r.cant_a;
+                            WriteLn('Cantidad de niños: ',r.cant_n,' Cantidad de adultos: ',r.cant_a);
+                            leer(f,r);
+                        End;
+                    total_n := total_n + total_l_n;
+                    total_a := total_a + total_l_a;
+                    WriteLn('Total niños en localiad ',aux_localidad,': ',total_l_n,' Total adultos en localidad ',aux_localidad,': ',total_l_a);
+                End;
+            WriteLn('Total niños en partido ',aux_partido,': ',total_n,' Total adultos en partido ',aux_partido,': ',total_a);    
+        End;
+      Close(f);  
+End;
+
+Var
   
-end.
+  f: arch;
+
+Begin
+  Assign(f,'informe');
+  listar(f);
+End.
---
+++ Practica 2/Ejercicio 5/generador5.pas
@@ -0,0 +1,16 @@
+
+Program genereador5;
+
+Type 
+    reg =   Record
+        partido:   string;
+        localidad:   string;
+        barrio:   string;
+        cant_n:   integer;
+        cant_a:   integer;
+    End;
+
+    arch =   file Of reg;
+Begin
+
+End.

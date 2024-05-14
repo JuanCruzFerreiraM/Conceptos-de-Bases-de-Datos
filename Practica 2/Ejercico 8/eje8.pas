3ed336e732a4b0d79e786b0c6fc133b8c91e610c
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2024-05-14 11:09:50 -0300

    Se actualiza lo hecho hasta el momento

---
+++ Practica 2/Ejercico 8/eje8.pas
@@ -0,0 +1,22 @@
+
+Program eje8;
+
+const
+    VALORALTO = 9999;
+    CANTIDAD = 15;
+    
+Type
+    reg_det = record
+        cod_zona: integer;
+        nombre_zona: string;
+        descripcion: string;
+        fecha: string;
+        cantidad_m: string; 
+    end;    
+
+    
+
+
+
+Begin
+End.
